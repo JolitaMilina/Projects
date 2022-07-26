a6755d9f9e6caa75752280313a9770e374ecf223
Jolita <jolita@test.lt>
2022-07-26 21:58:36 +0300

    js1Lesson

---
+++ Scripts/script.js
@@ -0,0 +1,26 @@
+alert ("hello world")
+
+let input= prompt;
+prompt("Enter the 2 numeric values")
+
+let a= 3;
+let b= 7;
+let result = a + b;
+console.log("The result of addition is "+ result);
+
+result= a-b;
+console.log("The result of subtraction is "+ result);
+
+result= a/b;
+alert ("the result of division is " + result);
+
+result= a*b;
+alert("the result of multiplication is " + result)
+
+
+prompt("Enter you first name and last name");
+
+let firstName= "Jolita"
+let lastName= "Milina"
+result= firstName + " " + lastName;
+alert("Hello " + result);
